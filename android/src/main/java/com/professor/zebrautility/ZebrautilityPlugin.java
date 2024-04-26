96f89744aa25bf53174692d365af249577eb7917
AnthonyZitoTegoshi-Evosystems <131671775+AnthonyZitoTegoshi-Evosystems@users.noreply.github.com>
2024-04-26 11:28:18 -0300

    Update ZebrautilityPlugin.java

--- android/src/main/java/com/professor/zebrautility/ZebrautilityPlugin.java
+++ android/src/main/java/com/professor/zebrautility/ZebrautilityPlugin.java
@@ -5,6 +5,22 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
+import java.util.Base64;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Looper;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
@@ -23,6 +39,16 @@ public class ZebrautilityPlugin implements FlutterPlugin, MethodCallHandler, Act
     private ActivityPluginBinding binding;
     private ArrayList<Printer> printers = new ArrayList<>();
 
+    private void convertBase64ImageToZPLString(String data, int rotation, MethodChannel.Result result) {
+        try {
+            byte[] decodedString = Base64.decode(data, Base64.DEFAULT);
+            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            result.success(Printer.getZplCode(decodedByte, false, rotation));
+        } catch (Exception e) {
+            result.error("-1", "Error", null);
+        }
+    }
+
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
         channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "zebrautility");
@@ -36,8 +62,10 @@ public class ZebrautilityPlugin implements FlutterPlugin, MethodCallHandler, Act
             Printer printer = new Printer(binding, flutterPluginBinding.getBinaryMessenger());
             result.success(printer.toString());
             printers.add(printer);
+        } else if (call.method.equals("convertBase64ImageToZPLString")) {
+            convertBase64ImageToZPLString(call.argument("Data").toString()
+                    , Integer.valueOf(call.argument("rotation").toString()), result);
         }
-
     }
 
 
